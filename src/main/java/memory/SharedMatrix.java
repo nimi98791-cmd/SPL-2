8ddaa6dfb9217ca9689fe20db5c0ff4522087336
iftahro <ifttah@gmail.com>
2025-12-20 14:56:09 +0200

    Shared Matrix acquire/release all

--- src/main/java/memory/SharedMatrix.java
+++ src/main/java/memory/SharedMatrix.java
@@ -47,18 +47,34 @@ public class SharedMatrix {
     }
 
     private void acquireAllVectorReadLocks(SharedVector[] vecs) {
-        // TODO: acquire read lock for each vector
+        for (SharedVector vec : vecs) {
+            if (vec != null) {
+                vec.readLock();
+            }
+        }
     }
 
     private void releaseAllVectorReadLocks(SharedVector[] vecs) {
-        // TODO: release read locks
+        for (SharedVector vec : vecs) {
+            if (vec != null) {
+                vec.readUnlock();
+            }
+        }
     }
 
     private void acquireAllVectorWriteLocks(SharedVector[] vecs) {
-        // TODO: acquire write lock for each vector
+        for (SharedVector vec : vecs) {
+            if (vec != null) {
+                vec.writeLock();
+            }
+        }
     }
 
     private void releaseAllVectorWriteLocks(SharedVector[] vecs) {
-        // TODO: release write locks
+        for (SharedVector vec : vecs) {
+            if (vec != null) {
+                vec.writeUnlock();
+            }
+        }
     }
 }
